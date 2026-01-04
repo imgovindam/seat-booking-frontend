a71f788e041c2a49ef79d8ea8dcc5a1011e8c570
imgovindam <govindraj902@gmail.com>
2026-01-04 19:15:00 +0530

    Fix component import path for production build

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-
-
-// import React from 'react'
-
-
-// import GetSeat from './Componenets/getSeat.jsx';
-
-// const App = () => {
-//   return (
-//     <div>
-//       <GetSeat/>
-//     </div>
-//   )
-// }
-
-// export default App
-
-
-
-import React from 'react';
-import GetSeat from './Components/getSeat.jsx';  // ✅ Fixed folder name
+import GetSeat from "./components/GetSeat";
 
 const App = () => {
   return (
     <div>
-      <GetSeat/>
+      <GetSeat />
     </div>
   );
 };
